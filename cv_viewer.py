cdd004701a4cdc099306cf21ac28c0e8bede085a
agolubev <andpgolubev@gmail.com>
2022-11-27 18:41:44 +0100

    Teach CV viewer to run user code

--- cv_viewer.py
+++ cv_viewer.py
@@ -4,8 +4,23 @@ import os
 import argparse
 import cv2
 import numpy as np
+import importlib
+import jurigged
+
+# importing util
+from contextlib import contextmanager
+@contextmanager
+def import_from(rel_path):
+    """Add module import relative path to sys.path"""
+    import sys
+    import os
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(cur_dir, rel_path))
+    yield
+    sys.path.pop(0)
 
 ENABLE_INTROSPECTION = False
+ENABLE_FULL_IMAGE_PROCESSING = False
 KERNEL_SIZE = 11
 
 DRAW_BUFFER = None
@@ -61,12 +76,12 @@ def create_text_window(redraw=False):
     putText('ESC : closes the viewer')
     putText('h : open this help window')
     putText('s : show/hide kernel at cursor (now: {k}x{k})'.format(k=KERNEL_SIZE))
+    putText('p : show/hide full image processing'.format(k=KERNEL_SIZE))
     putText('+ : increases kernel size under cursor')
     putText('- : decreases kernel size under cursor')
     putText.add_blank(2)
     putText('Cool features:')
     putText('i : open info window')
-    putText('r : reload user code (outputs into info window)')
     cv2.imshow(window, img)
 
 def create_info_window(redraw=False):
@@ -85,19 +100,14 @@ def create_info_window(redraw=False):
 
     cv2.imshow(window, img)
 
-def introspection_main(event, x, y, flags, img):
+def handle_mouse_event(event, x, y, flags, img):
     global ENABLE_INTROSPECTION
     global DRAW_BUFFER
     if not ENABLE_INTROSPECTION:
         return
 
     # clean up the draw buffer first to avoid previous frame artifacts
-    DRAW_BUFFER = img.copy()
-
-    half = int(KERNEL_SIZE / 2)
-    cv2.rectangle(DRAW_BUFFER,
-        (x - half, y - half), (x + half, y + half),
-        (0, 0, 255), 2)
+    # DRAW_BUFFER = img.copy()
 
     global CURSOR
     CURSOR.x = x
@@ -110,8 +120,9 @@ class InfoOutput:
     def commit(self):
         global INFO_BUFFER
         import copy
-        INFO_BUFFER = copy.deepcopy(self._buf)
-        self._buf.clear()
+        if len(self._buf) > 0:
+            INFO_BUFFER = copy.deepcopy(self._buf)
+            self._buf.clear()
 
     def add(self, *objects, sep=' '):
         import io
@@ -121,7 +132,14 @@ class InfoOutput:
         output.close()
         self._buf.append(line)
 
-def compute_statistics_on_image(img, output_buffer: InfoOutput):
+def invoke_whole_image_processor(img, user_script, output_buffer: InfoOutput):
+    global DRAW_BUFFER
+    if user_script:
+        src_image = img
+        dst_image = DRAW_BUFFER
+        DRAW_BUFFER = user_script.process_image(src_image, dst_image, output_buffer)
+
+def invoke_roi_processor(img, user_script, output_buffer: InfoOutput):
     def clip(x, dim):
         return np.clip(x, 0, img.shape[dim])
 
@@ -136,17 +154,42 @@ def compute_statistics_on_image(img, output_buffer: InfoOutput):
     roi = img[y_f:y_l, x_f:x_l]
 
     # mean value:
-    mean = np.mean(roi, axis=(0, 1))
-    mean = [int(e) for e in mean]
-    output_buffer.add('Window mean:', mean)
+    # mean = np.mean(roi, axis=(0, 1))
+    # mean = [int(e) for e in mean]
+    # output_buffer.add('Window mean:', mean)
+
+    if user_script:
+        src_roi = roi
+        dst_roi = DRAW_BUFFER[y_f:y_l, x_f:x_l]
+        user_script.process_region(src_roi, dst_roi, output_buffer)
+
+    # show roi
+    thickness = 2
+    half += thickness # extra spacing for rectangle lines
+    cv2.rectangle(DRAW_BUFFER,
+        (x - half, y - half), (x + half, y + half),
+        (0, 0, 255), thickness)
+
+def import_module(path):
+    spec = importlib.util.spec_from_file_location('user_script', path)
+    user_code = importlib.util.module_from_spec(spec)
+    sys.modules['user_script'] = user_code
+    print(dir(importlib.machinery))
+    if os.path.exists(user_code.__cached__):
+        os.remove(user_code.__cached__)
+    spec.loader.exec_module(user_code)
+    return user_code
 
 
-def handle_key(key, user_code_path):
+def handle_key(key, user_script_path):
     global ENABLE_INTROSPECTION
+    global ENABLE_FULL_IMAGE_PROCESSING
     global KERNEL_SIZE
 
     if key == ord('s'):
         ENABLE_INTROSPECTION = not ENABLE_INTROSPECTION
+    if key == ord('p'):
+        ENABLE_FULL_IMAGE_PROCESSING = not ENABLE_FULL_IMAGE_PROCESSING
 
     if key == ord('+'):
         size = KERNEL_SIZE
@@ -163,16 +206,19 @@ def handle_key(key, user_code_path):
     if key == ord('i'):
         create_info_window(redraw=True)
 
-    # if key == ord('r') and user_code_path:
-    #     import importlib
-    #     importlib.reload()
-
 
 def main():
+    global ENABLE_FULL_IMAGE_PROCESSING
     global ENABLE_INTROSPECTION
     global DRAW_BUFFER
 
     args = parse_arguments()
+
+    user_script = None
+    if args.input_enhancer:
+        user_script_path = os.path.realpath(args.input_enhancer)
+        user_script = import_module(user_script_path)
+        jurigged.watch(user_script_path)
 
     original = cv2.imread(args.image_file)
     # Note: .copy() changes the data pointer! (so we need global variable)
@@ -181,7 +227,7 @@ def main():
     window = 'cv_viewer'
     cv2.namedWindow(window, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window, 1920, 1080)
-    cv2.setMouseCallback(window, introspection_main, original)
+    cv2.setMouseCallback(window, handle_mouse_event, original)
 
     create_text_window()
 
@@ -190,14 +236,21 @@ def main():
         key = cv2.waitKey(30)
         if key == 27: # ESC
             break
-        handle_key(key, args.input_enhancer)
-        if not ENABLE_INTROSPECTION:
-            DRAW_BUFFER = original.copy()
-        else:
-            adaptor = InfoOutput()
-            compute_statistics_on_image(original, adaptor)
-            adaptor.commit()
-            create_info_window()
+        handle_key(key, user_script_path)
+
+        DRAW_BUFFER = original.copy()
+        if not ENABLE_FULL_IMAGE_PROCESSING and not ENABLE_INTROSPECTION:
+            continue
+
+        adaptor = InfoOutput()
+        if ENABLE_FULL_IMAGE_PROCESSING:
+            invoke_whole_image_processor(original, user_script, adaptor)
+        if ENABLE_INTROSPECTION:
+            invoke_roi_processor(original, user_script, adaptor)
+
+        adaptor.commit()
+
+        create_info_window()
 
 
 if __name__ == '__main__':
---
+++ user_code.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+# output_buffer has an .add() method to write out a single user code line
+def process_region(src_region, dst_region, output_buffer):
+    mean = np.mean(dst_region, axis=(0, 1))
+    if isinstance(mean, float):
+        mean = [mean]
+    mean = [int(e) for e in mean]
+    output_buffer.add('Window mean:', mean)
+    cv2.blur(dst_region, ksize=(11, 11), dst=dst_region)
+    # cv2.Canny(dst_region, 100, 200, edges=dst_region)
+
+# return dst_image even if it is unchanged
+def process_image(src_image, dst_image, output_buffer):
+    dst_image = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY)
+    return dst_image
